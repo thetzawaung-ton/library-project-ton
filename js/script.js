87d67ceb89ecd099859bc4543d1b47855cc4e710
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2026-02-11 09:22:01 +0630

    Add function to display books in a table

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const myLibrary = [];
+const myLibrary = [
+    {title: 'Nway', author: 'Ohn', pages: 223, readStatus: 'read'}
+];
 
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -7,7 +9,33 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
+const table = document.querySelector('table');
+const tbody = document.querySelector('tbody');
+
 function addBookToLibrary(title, author, pages, readStatus) {
     const newBook = new Book(title, author, pages, readStatus);
     myLibrary.push(newBook);
+    checkLibrary();
 }
+
+function checkLibrary() {
+    while(tbody.firstChild) {
+        tbody.firstChild.remove();
+    }
+    myLibrary.forEach(function(book) {
+    const bookrow = document.createElement('tr');
+    const title = document.createElement('td');
+    const author = document.createElement('td');
+    const pages = document.createElement('td');
+    const readStatus = document.createElement('td');
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    readStatus.textContent = book.readStatus;
+
+    bookrow.append(title, author, pages, readStatus);
+    tbody.appendChild(bookrow);
+    })
+}
+checkLibrary();
